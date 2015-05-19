feddc76a4876e71a2bfd3c66ac58ba283c5b9eae
aGiustini <agiustinic@hotmail.com>
2015-05-19 05:07:43 +0200

    app.js - Calendario final

--- Natureadventure/src/main/webapp/html/monitor/src/app.js
+++ Natureadventure/src/main/webapp/html/monitor/src/app.js
@@ -2,20 +2,19 @@ angular.module('mwl.calendar', []);
 
 var app = angular.module('app', ['mwl.calendar', 'ui.bootstrap'])
 
+// Definición del controlador del Calendario
 app.controller('MainCtrl', ['$scope', '$modal', 'CalendarioService', 'moment',
                             function ($scope, $modal, CalendarioService, moment) {
 	
-	app.baseURI = 'http://localhost:8080/Natureadventure/gerente/reservas/aceptadas';
+	app.baseURI = 'http://localhost:8080/Natureadventure/monitor/reservas/';
 	var self = this;
 
     var currentYear = moment().year();
     var currentMonth = moment().month();
     
     /*
-     * Datos de reservas de prueba
-     *
-     */
-    
+	//Datos de reservas de prueba
+ 
     var reservas = {"reserva":
     				[{"fechaActividad": "8-05-2015", "fechaReserva": "8-05-2015", "cantidadPersonas": 4,
     				"precio": 35, "nombre": "Monitor1","apellidos": "ApellidosMonitor1", "dni": "20875643S",
@@ -41,13 +40,12 @@ app.controller('MainCtrl', ['$scope', '$modal', 'CalendarioService', 'moment',
         				"precio": 35, "nombre": "Monitor2","apellidos": "ApellidosMonitor2", "dni": "20875643S",
         				"correo": "correo@monitor2.com", "telefono": 654321987, "estado": "aceptada",
         				"actividad": 1, "usuario": 1, "contrato": "contrato"}]}
-
-    /*
-    var reservas = CalendarioService.retrieveAllSupervisar()
-	.success(function(data){
-		reservas = data;
+	*/
+    
+    $scope.reservas = CalendarioService.retrieveAllSupervisar()
+	.success(function(data) {
+		$scope.reservas = data.reserva;
 	});
-    */
     
     $scope.calendarView = 'month';
     $scope.calendarDay = new Date();
@@ -55,21 +53,23 @@ app.controller('MainCtrl', ['$scope', '$modal', 'CalendarioService', 'moment',
 	
 	];
 	
+    // Obtener el nombre del monitor que ha iniciado sesión
 	$scope.username =  window.location.href.slice(window.location.href.indexOf('?') + 1).split('=')[1];
 
-	for (var i = 0; i < reservas.reserva.length; i++) {
-		if (reservas.reserva[i].nombre == $scope.username) {
+	// Rellenar el vector de eventos a mostrar en el calendario
+	for (var i = 0; i < $scope.reservas.length; i++) {
+		if ($scope.reservas[i].nombre == $scope.username) {
 			var tmpEvent = {
 				title: "Reserva para "+$scope.username,
 				type: "important",
-				starts_at: new Date(reservas.reserva[i].fechaActividad.split("-")[2],
-									reservas.reserva[i].fechaActividad.split("-")[1]-1,
-									reservas.reserva[i].fechaActividad.split("-")[0],
+				starts_at: new Date($scope.reservas[i].fechaActividad.split("-")[2],
+									$scope.reservas[i].fechaActividad.split("-")[1]-1,
+									$scope.reservas[i].fechaActividad.split("-")[0],
 									8, 30),
-				ends_at: new Date(reservas.reserva[i].fechaActividad.split("-")[2],
-									reservas.reserva[i].fechaActividad.split("-")[1]-1,
-									reservas.reserva[i].fechaActividad.split("-")[0],
-									9,30)
+				ends_at: new Date($scope.reservas[i].fechaActividad.split("-")[2],
+								  $scope.reservas[i].fechaActividad.split("-")[1]-1,
+								  $scope.reservas[i].fechaActividad.split("-")[0],
+								  9,30)
 			}
 			$scope.events.push(tmpEvent);
 		}
@@ -89,16 +89,18 @@ app.controller('MainCtrl', ['$scope', '$modal', 'CalendarioService', 'moment',
   }]);
 
 
+// Servicios web Calendario
+
 app.service('CalendarioService', ['$http', function($http) {
-	
-	this.retrieveReservasAceptadas = function() {
-		var url = app.baseURI;
-		return $http.get(url);
-	};
 	
 	this.retrieveAllSupervisar = function() {
 		var user = window.location.href.slice(window.location.href.indexOf('?') + 1).split('=')[1];
 		var url = app.baseURI+"supervisar/"+user;
+		return $http.get(url);
+	};
+	
+	this.retrieveReservasAceptadas = function() {
+		var url = app.baseURI;
 		return $http.get(url);
 	};
 
