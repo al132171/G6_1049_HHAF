fa1a221f384549c13d7e95bd3820b7a8a1e4e11b
Héctor Saiz Sánchez <al187091@uji.es>
2015-05-31 01:18:00 +0200

    Comprobación de que la fecha de reserva es una fecha real y de que el formato es correcto

--- Natureadventure/src/main/webapp/js/serviceActividadCliente.js
+++ Natureadventure/src/main/webapp/js/serviceActividadCliente.js
@@ -86,6 +86,7 @@
         $scope.show.informacionActividad = false;
         $scope.show.cantidadResultados = 0;
         $scope.show.precio = 0;
+        $scope.show.formatoFechaValido = true;
 
 
 
@@ -303,15 +304,90 @@
         }
         
         //  Calcula el precio de la reserva en función de el numero de participantes y le añade el 21% de IVA
-        $scope.calculaPrecio = function(numeroAsistentes){
+        $scope.calculaPrecio = function (numeroAsistentes){
             
             var precio = $scope.feed.datosActuales.precio * numeroAsistentes * 1.21;
             $scope.show.precio = precio.toFixed(2);
         };
+
+        
+        $scope.validaFechaDDMMAAAA = function (fecha){
+            var dtCh= "-";
+            var minYear=1900;
+            var maxYear=2100;
+            function isInteger(s){
+                var i;
+                for (i = 0; i < s.length; i++){
+                    var c = s.charAt(i);
+                    if (((c < "0") || (c > "9"))) return false;
+                }
+                return true;
+            }
+            function stripCharsInBag(s, bag){
+                var i;
+                var returnString = "";
+                for (i = 0; i < s.length; i++){
+                    var c = s.charAt(i);
+                    if (bag.indexOf(c) == -1) returnString += c;
+                }
+                return returnString;
+            }
+            function daysInFebruary (year){
+                return (((year % 4 == 0) && ( (!(year % 100 == 0)) || (year % 400 == 0))) ? 29 : 28 );
+            }
+            function DaysArray(n) {
+                for (var i = 1; i <= n; i++) {
+                    this[i] = 31
+                    if (i==4 || i==6 || i==9 || i==11) {this[i] = 30}
+                    if (i==2) {this[i] = 29}
+                }
+                return this
+            }
+            function isDate(dtStr){
+                var daysInMonth = DaysArray(12)
+                var pos1=dtStr.indexOf(dtCh)
+                var pos2=dtStr.indexOf(dtCh,pos1+1)
+                var strDay=dtStr.substring(0,pos1)
+                var strMonth=dtStr.substring(pos1+1,pos2)
+                var strYear=dtStr.substring(pos2+1)
+                strYr=strYear
+                
+                for (var i = 1; i <= 3; i++) {
+                    if (strYr.charAt(0)=="0" && strYr.length>1) strYr=strYr.substring(1)
+                        }
+                month=parseInt(strMonth)
+                day=parseInt(strDay)
+                year=parseInt(strYr)
+                if (pos1==-1 || pos2==-1){
+                    return false
+                }
+                if (strMonth.length!=2 || month<1 || month>12){
+                    return false
+                }
+                if (strDay.length!=2 || day<1 || day>31 || (month==2 && day>daysInFebruary(year)) || day > daysInMonth[month]){
+                    return false
+                }
+                if (strYear.length != 4 || year==0 || year<minYear || year>maxYear){
+                    return false
+                }
+                if (dtStr.indexOf(dtCh,pos2+1)!=-1 || isInteger(stripCharsInBag(dtStr, dtCh))==false){
+                    return false
+                }
+                return true
+            }
+            if(isDate(fecha)){
+                $scope.show.formatoFechaValido = true;
+                alert("correcta");
+            }else{
+                $scope.show.formatoFechaValido = false;
+                $scope.formularioReserva.fechaActividad.$invalid = true;
+                alert("incorrecta");
+            }
+};
         
 
         //		Limpiar el formulario para que si sales de la ventana modal se limpien los mensajes de error y formato
-        $scope.resetFormReserva = function(reserva){ 
+        $scope.resetFormReserva = function(reserva){
 
             var month = new Array(12);
             month[0] = "01";
